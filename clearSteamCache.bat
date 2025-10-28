79325109fcc70aa850414364a3bd71fcc9c25c79
arcanine300 <ikeellyou.dkh@gmail.com>
2025-10-27 20:08:14 -0400

    Removed redundant directory prefix

--- clearSteamCache.bat
+++ clearSteamCache.bat
@@ -18,28 +18,30 @@ REG DELETE HKEY_CURRENT_USER\Software\Classes\steam /f
 REG DELETE HKEY_CLASSES_ROOT\steam /f
 cd\
 
+::Change the following line to your Steam install directory
 cd C:\Program Files (x86)\Steam
-del /s /q d:\C:\Program Files (x86)\Steam\appcache\Steam.log
-del /S /Q "C:\Program Files (x86)\Steam\config"
-del /S /q d:\C:\Steam\appcache\AppUpdateStats.blob
-del /S /q d:\C:\Steam\appcache\GameOverlayRenderer.log
-del /S /q d:\C:\Steam\appcache\GameOverlayUI.exe.log
-del /S /q d:\C:\Steam\appcache\crashhandler.dll
-del /S /q d:\C:\Steam\appcache\crashhandler64.dll
-del /S /q d:\C:\Steam\appcache\CSERHelper.dll
-del /S /q d:\C:\Steam\appcache\Steam.dll
-del /S /q d:\C:\Steam\appcache\steamclient.dll
-del /S /q d:\C:\Steam\appcache\steamclient64.dll
-del /S /q d:\C:\Steam\appcache\SteamUI.dll
-del /S /q d:\C:\Steam\appcache\streaming_client.exe
-del /S /q d:\C:\Steam\appcache\WriteMiniDump.exe
-del /S /q d:\C:\Steam\appcache\debug.log
+
+del /s /q d:\C:\appcache\Steam.log
+del /S /q d:\C:\appcache\AppUpdateStats.blob
+del /S /q d:\C:\appcache\GameOverlayRenderer.log
+del /S /q d:\C:\appcache\GameOverlayUI.exe.log
+del /S /q d:\C:\appcache\crashhandler.dll
+del /S /q d:\C:\appcache\crashhandler64.dll
+del /S /q d:\C:\appcache\CSERHelper.dll
+del /S /q d:\C:\appcache\Steam.dll
+del /S /q d:\C:\appcache\steamclient.dll
+del /S /q d:\C:\appcache\steamclient64.dll
+del /S /q d:\C:\appcache\SteamUI.dll
+del /S /q d:\C:\appcache\streaming_client.exe
+del /S /q d:\C:\appcache\WriteMiniDump.exe
+del /S /q d:\C:\appcache\debug.log
 del /Q F logs
 del /Q F *.mdmp
 del ClientRegistry.blob
-rmdir /S /Q "C:\Program Files (x86)\Steam\appcache"
-rmdir /S /Q "C:\Program Files (x86)\Steam\config"
-rmdir /S /Q "C:\Program Files (x86)\Steam\userdata"
-rmdir /S /Q "C:\Program Files (x86)\Steam\dumps"
-rmdir /S /Q "C:\Program Files (x86)\Steam\logs"
-rmdir /S /Q "C:\Program Files (x86)\Steam\appcache"
+
+rmdir /S /Q "C:\appcache"
+rmdir /S /Q "C:\config"
+rmdir /S /Q "C:\userdata"
+rmdir /S /Q "C:\dumps"
+rmdir /S /Q "C:\logs"
+rmdir /S /Q "C:\appcache"
